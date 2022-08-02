0fb7f7b9517df4364b843a592f068cfca828571e
Prashant Bhole <prashantdbhole@gmail.com>
2022-08-02 13:46:04 +0530

    Updated run.sh script to install pytest and create virtual env

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+export WORKSPACE=`pwd`
+
+# Create/Activate virtualenv
+virtualenv testenv -p /path/to/python/bin
+source testenv/bin/activate
+
+pip install -U pytest
+
 pytest -v -m "sanity" --html=Reports/report.html testCases/ --browser chrome
 # pytest -v -m "sanity and regression" --html=Reports/report.html testCases/ --browser chrome
 # pytest -v -m "sanity or regression" --html=Reports/report.html testCases/ --browser chrome
