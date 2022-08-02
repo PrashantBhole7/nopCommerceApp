59deb2e7104a244eba8d5be1f566bbb7a7b024f8
Prashant Bhole <prashantdbhole@gmail.com>
2022-08-02 15:14:24 +0530

    Small change in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 source venv/bin/activate
-python -m pytest -v -m "sanity" --html=Reports/report.html testCases/ --browser chrome
+
+pytest -v -m "sanity" --html=Reports/report.html testCases/ --browser chrome
 # pytest -v -m "sanity and regression" --html=Reports/report.html testCases/ --browser chrome
 # pytest -v -m "sanity or regression" --html=Reports/report.html testCases/ --browser chrome
 # pytest -v -m "regression" --html=Reports/report.html testCases/ --browser chrome
