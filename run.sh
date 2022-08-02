dbe9937fa8a0479ff213973f7dbbf2b8d9e70860
Prashant Bhole <prashantdbhole@gmail.com>
2022-08-02 15:11:54 +0530

    Updated run.sh and after running some test cases file chages

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-pip3 --install --upgrade pip
-pip3 install pytest
-
+source venv/bin/activate
 python -m pytest -v -m "sanity" --html=Reports/report.html testCases/ --browser chrome
 # pytest -v -m "sanity and regression" --html=Reports/report.html testCases/ --browser chrome
 # pytest -v -m "sanity or regression" --html=Reports/report.html testCases/ --browser chrome
