12dc49423050fecb790371048750cb7966d1ed4e
Prashant Bhole <prashantdbhole@gmail.com>
2022-08-02 14:04:54 +0530

    Modifing run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-export WORKSPACE=`pwd`
+pip3 --install --upgrade pip
+pip3 install pytest
 
-# Create/Activate virtualenv
-virtualenv testenv -p /path/to/python/bin
-source testenv/bin/activate
-
-pip install -U pytest
-
-pytest -v -m "sanity" --html=Reports/report.html testCases/ --browser chrome
+python -m pytest -v -m "sanity" --html=Reports/report.html testCases/ --browser chrome
 # pytest -v -m "sanity and regression" --html=Reports/report.html testCases/ --browser chrome
 # pytest -v -m "sanity or regression" --html=Reports/report.html testCases/ --browser chrome
 # pytest -v -m "regression" --html=Reports/report.html testCases/ --browser chrome
